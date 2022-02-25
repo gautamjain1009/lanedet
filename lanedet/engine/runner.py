53bb1a0d89c182bc88a9ff091da950d326e413de
gautamjain1009 <gautamjain1009@gmail.com>
2022-02-25 18:32:36 +0200

    Adding RSC for only SCNN

--- lanedet/engine/runner.py
+++ lanedet/engine/runner.py
@@ -16,6 +16,8 @@ from lanedet.utils.recorder import build_recorder
 from lanedet.utils.net_utils import save_model, load_network
 from mmcv.parallel import MMDataParallel 
 
+from lanedet.models.nets.detector import *
+from lanedet.models.heads.lane_seg import *
 
 class Runner(object):
     def __init__(self, cfg):
@@ -25,14 +27,17 @@ class Runner(object):
         self.cfg = cfg
         self.recorder = build_recorder(self.cfg)
         self.net = build_net(self.cfg)
+        
         # for name, module in self.net.named_modules():
         #     print(name)
         # print(self.net.Detector)
-        # self.net.to(torch.device('cuda'))
+        self.net.to(torch.device('cuda'))
         # self.net = torch.nn.parallel.DataParallel(
         #         self.net, device_ids = range(self.cfg.gpus)).cuda()
-        self.net = MMDataParallel(
-                self.net, device_ids = range(self.cfg.gpus)).cuda()
+        
+        # self.net = MMDataParallel(
+        #         self.net, device_ids = range(self.cfg.gpus)).cuda()
+
         self.recorder.logger.info('Network: \n' + str(self.net))
         self.resume()
         self.optimizer = build_optimizer(self.cfg, self.net)
@@ -67,44 +72,68 @@ class Runner(object):
                 break
             date_time = time.time() - end
             self.recorder.step += 1
-            data = self.to_cuda(data)
-            output = self.net(data)
+
+            # data = self.to_cuda(data)
+            
+            for k in data:
+                data[k] = data[k].cuda()
+            # # output = self.net(data)
+            
             self.optimizer.zero_grad()
             
-            print(output.keys())
-            print(data.keys())
-                    
+            ###################################### 
+            all_f = self.net.featurizer(data['img']) ### list of Resnet layers out
+            # print(all_f[0].shape)
             
+            output = self.net.classifier(all_f, data) # (batch_size,classes,h,W)
+            all_p = output['outs']
+            # print(all_p['seg'].shape)
+
+            # # print(output.keys())
+            # # print(data['img'].shape) # --- [8,3,368,640]
+                    
             #TODO: Implement the RSC algorithm and check it enhances the accuracy. 
             """
             RSC algorithm: https://github.com/facebookresearch/DomainBed/blob/25f173caa689f20828629b2e42f90193f203fdfa/domainbed/algorithms.py#L866
             """
-            bbone_features = output['bbone_outs']
-            print(bbone_features.shape)
-            # a = output['outs']
-            # # print(a['seg'].shape)
-            # print( data['mask'].shape)
-            # # Equation (1): compute gradients with respect to representation            
-            # all_g = grad((output['outs'] * data['mask']).sum(), bbone_features) ## this loss can be manually calcualted also using outs *
-            # # all_g_0 = all_g[0]
-            # print(all_g.shape)
+            
+            all_p = torch.max(all_p['seg'], 1)[0]
+            print("===> predictions shape", all_p.shape)
+            
+            print(data['mask'].shape)
+            def muted_gradients(all_p, data, all_f):
+                # Equation (1): compute gradients with respect to representation            
+                all_g = grad((all_p * data['mask']).sum(), all_f,retain_graph=True) ## this loss can be manually calcualted also using outs *
+                all_g_0 = all_g[0]
+                print("===>Gradient_shape", all_g_0.shape)
 
+                # Equation (2): compute top-gradient-percentile mask
+                #TODO: set the hyperparam for muting the gradients drop_f
+                
+                percentiles = np.percentile(all_g_0.cpu(), 33, axis =0) ## TODO:confirm does axis =0 means percentile taken along batches 
+                percentiles = torch.Tensor(percentiles)
+                print(percentiles.shape)
+                percentiles = percentiles.unsqueeze(0).repeat(8,1,1,1)
 
-            # # Equation (2): compute top-gradient-percentile mask
-            # #TODO: set the hyperparam for muting the gradients drop_f
-            # precentiles = np.percentile(all_g.cpu(), 33, axis =1)
-            # percentiles = torch.Tensor(percentiles)
-            # percentiles = percentiles.unsqueeze(1).repeat(1, all_g.size(1))
+                maskf = all_g_0.lt(percentiles.cuda()).float()
+                print("** printing masks shapes")
+                print(maskf.shape)
+                
+                # Equation (3): mute top-gradient-percentile activations
+                # all_f_muted = [maskf * bbone_f for bbone_f in bbone_features]
+                all_f_muted = maskf * all_f
+                
+                return all_f_muted
 
-            # maskf = all_g.lt(precentiles.cuda()).float()
-
-            #  # Equation (3): mute top-gradient-percentile activations
-            #  all_f_muted = 
-
-
-
-            loss = output['loss']
+            all_f_muted = [muted_gradients(all_p, data, layerwise_features) for layerwise_features in all_f]
+          
+            # Equation (4): compute muted predictions
+            muted_output = self.net.classifier(all_f_muted, data)
+            
+            loss = muted_output['loss']
+            ##############################################
             loss.backward()
+            
             self.optimizer.step()
             if not self.cfg.lr_update_by_epoch:
                 self.scheduler.step()
@@ -112,7 +141,7 @@ class Runner(object):
                 self.warmup_scheduler.dampen()
             batch_time = time.time() - end
             end = time.time()
-            self.recorder.update_loss_stats(output['loss_stats'])
+            self.recorder.update_loss_stats(muted_output['loss_stats'])
             self.recorder.batch_time.update(batch_time)
             self.recorder.data_time.update(date_time)
 
@@ -144,11 +173,20 @@ class Runner(object):
         self.net.eval()
         predictions = []
         for i, data in enumerate(tqdm(self.val_loader, desc=f'Validate')):
-            data = self.to_cuda(data)
+            for k in data:
+                data[k] = data[k].cuda()
+            
             with torch.no_grad():
-                output = self.net(data)
-                output = self.net.module.get_lanes(output)
+                # output = self.net(data)
+                # output = self.net.module.get_lanes(output)
+                all_f = self.net.featurizer(data['img']) ### list of Resnet layers out
+                
+                output = self.net.classifier(all_f, data) # (batch_size,classes,h,W)               
+                
+
+                output = self.net.get_lanes(output)
                 predictions.extend(output)
+            
             if self.cfg.view:
                 self.val_loader.dataset.view(output, data['meta'])
 
--- lanedet/models/nets/detector.py
+++ lanedet/models/nets/detector.py
@@ -14,28 +14,68 @@ class Detector(nn.Module):
         self.aggregator = build_aggregator(cfg) if cfg.haskey('aggregator') else None
         self.neck = build_necks(cfg) if cfg.haskey('neck') else None
         self.heads = build_heads(cfg)
-    
+
     def get_lanes(self, output):
         return self.heads.get_lanes(output)
 
-    def forward(self, batch):
-        output = {}
-        fea = self.backbone(batch['img'])
+    def featurizer(self, data):
+        print("To check if i am in the function::",data.shape)
+        fea = self.backbone(data)
+        
+        return fea
 
+    def classifier(self,fea, batch):
+        output = {}
+        
         if self.aggregator:
             fea[-1] = self.aggregator(fea[-1])
-
+        
         if self.neck:
             fea = self.neck(fea)
 
         if self.training:
-            
-            output['bbone_outs'] = fea
-            
             out = self.heads(fea, batch=batch)
-            output.update(self.heads.loss(out, batch))
             output['outs'] = out
+            output.update(self.heads.loss(out, batch))
         else:
             output = self.heads(fea)
 
         return output
+
+    # def forward(self, batch):
+        
+    #     output = {}
+        
+    #     fea = self.featurizer(batch)
+    #     output['bbone_outs'] = fea
+
+    #     if self.training:
+    #         out = self.classifier(fea, batch)
+    #         output.update(self.heads.loss(out, batch))
+    #         output['outs'] = out
+
+    #     else:
+    #         output = self.classifier(fea, batch)
+
+    #     return output
+
+    # def forward(self, batch):
+        
+    #     output = {}
+    #     fea = self.backbone(batch['img'])
+        
+    #     output['bbone_outs'] = fea
+        
+    #     if self.aggregator:
+    #         fea[-1] = self.aggregator(fea[-1])
+    #     if self.neck:
+    #         fea = self.neck(fea)
+
+    #     if self.training:
+    #         out = self.heads(fea, batch=batch)
+    #         output.update(self.heads.loss(out, batch))
+    #         output['outs'] = out
+    #     else:
+    #         output = self.heads(fea)
+
+    #     return output
